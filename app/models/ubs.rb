bbf1950ada13180ce44e7d16f07be079f9539efa
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2018-06-02 18:53:10 -0300

    Added model to persist UBS

--- app/models/ubs.rb
+++ app/models/ubs.rb
@@ -1,2 +1,10 @@
 class Ubs < ApplicationRecord
+    require 'csv'
+
+    def self.import(file)
+        CSV.foreach(file.path, headers: true) do |row|
+          Ubs.create! rows.to_hash
+        end
+    end
+    
 end

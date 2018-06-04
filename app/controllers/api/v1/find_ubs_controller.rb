15ccd99c7540e15ae48c5a1101ec662966577e5f
Rodrigo Ferraz Azevedo <rodrigo.azevedo@sintesoft.com.br>
2018-06-04 11:39:21 -0300

    Added filter by latitude and longitude to find_ubs

--- app/controllers/api/v1/find_ubs_controller.rb
+++ app/controllers/api/v1/find_ubs_controller.rb
@@ -1,9 +1,16 @@
 module Api
   module V1
     class FindUbsController < ApplicationController
+      #List all UBS
       def index
         ubs = Ubs.order('created_at DESC');
-        render json: {status: 'SUCCESS', message: 'UBS List', data: ubs}, status: :ok
+        render json: {status: 'SUCCESS', message: 'UBS list', data: ubs}, status: :ok
+      end
+      #List UBS from Latitude/longitude values
+      def show
+        #ubs = Ubs.find(params[:id])
+        ubs = Ubs.where("(vlr_latitude = :lat) and (vlr_longitude = :lon)",{lat: params[:latitude], lon: params[:longitude]})
+        render json: {status: 'SUCCESS', message: 'UBS list based on latitude/longitude values', data: ubs}, status: :ok 
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   
   namespace 'api' do
     namespace 'v1' do
-      resources :find_ubs
+      #resources :find_ubs   
+      get 'find_ubs', to: 'find_ubs#index' #all values      
+      get 'find_ubs/:latitude/:longitude', to: 'find_ubs#show' , constraints: { latitude: /[^\/]+/, longitude: /[^\/]+/ } #specific values by latitude/longitude. 
+      # Constraint above: regex to enable dots because dot is used as a separator for formatted routes, then is unable for default  
     end
   end
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
